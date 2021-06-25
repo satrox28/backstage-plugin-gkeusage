df328b00668727f3c2bdd1b6c1914856598a43e5
Brett Wright <wright.brett@gmail.com>
2021-06-25 09:33:32 +0200

    fixed type

--- src/components/GKEUsageCost.tsx
+++ src/components/GKEUsageCost.tsx
@@ -50,7 +50,7 @@ export function GKECost(props: CostProps) {
         })
       );
 
-      const currency: any = getSymbolFromCurrency(json[0].currency);
+      const currency: string|undefined = getSymbolFromCurrency(json[0].currency);
 
       const totalCost =
         Math.round(
